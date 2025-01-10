be40a05cb6c5a7898fd788cef595f97e74f928ec
Saquib <saquibalam934@gmail.com>
2025-01-11 03:47:45 +0530

    Spring Security for Password Encryption

---
+++ src/main/java/com/hms/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hms.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        http.csrf().disable().cors().disable();
+        return http.build();
+    }
+}
